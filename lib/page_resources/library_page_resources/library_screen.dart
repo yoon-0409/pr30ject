89563a8d209ac3ea693cb246243c4843cf0db9ed
Hyun <hyun_7586@naver.com>
2024-05-07 19:02:32 +0900

    modify : library_screen.dart
    - 버튼 선택에 따라 상단바의 색상과 굵기가 바뀌는 기능 추가
    - 기타 컴포넌트의 크기 조정

--- lib/page_resources/library_page_resources/library_screen.dart
+++ lib/page_resources/library_page_resources/library_screen.dart
@@ -1,54 +1,123 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors,
 import 'package:flutter/material.dart';
+import 'package:pr30ject_modified/widget/book_widget.dart';
 
 class MyLibrary extends StatefulWidget {
-  final paddingSize;
+  final double paddingSize;
 
-  const MyLibrary({super.key, required this.paddingSize});
+  const MyLibrary({Key? key, required this.paddingSize}) : super(key: key);
 
   @override
   State<MyLibrary> createState() => _MyLibraryState();
 }
 
 class _MyLibraryState extends State<MyLibrary> {
+  int _selectedIndex = 0; // 선택된 버튼 인덱스를 저장할 변수
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
     var paddingSize = widget.paddingSize;
 
-    return Container(
-        padding: EdgeInsets.fromLTRB(paddingSize, 0, paddingSize, 0),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('검색!');
+            },
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(paddingSize, 10, paddingSize, 0),
         child: Column(
           children: [
             // 나의 서재 텍스트
             Flexible(
-                child: Container(
-                    child: Text('나의 서재', style: TextStyle(fontSize: 30)),
-                    alignment: Alignment.centerLeft),
-                flex: 1),
-
-            // 화면전환 버튼
-            Flexible(
-              child: Row(
-                children: [
-                  TextButton(onPressed: () {}, child: Text('전체')),
-                  TextButton(onPressed: () {}, child: Text('읽은 책')),
-                  TextButton(onPressed: () {}, child: Text('읽고 있는 책')),
-                  TextButton(onPressed: () {}, child: Text('읽고 싶은')),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              flex: 10,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '나의 서재',
+                  style: TextStyle(
+                    fontSize: screenSize.height * 0.045,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                  ),
+                ),
               ),
-              flex: 1,
+            ),
+            // 버튼 및 상단 일직선
+            Flexible(
+              flex: 10,
+              child: Column(
+                children: [
+                  // 상단 일직선 -> 버튼 상태에 따라 색상 변경
+                  Row(
+                    children: [
+                      _buildSegmentContainer(screenSize.width / 5, 0),
+                      _buildSegmentContainer(screenSize.width / 4.5, 1),
+                      _buildSegmentContainer(screenSize.width / 4.5, 2),
+                      _buildSegmentContainer(screenSize.width / 4.5, 3),
+                    ],
+                  ),
+                  // 화면전환 버튼
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildTextButton(0, '전체'),
+                      _buildTextButton(1, '읽은 책'),
+                      _buildTextButton(2, '읽고 있는 책'),
+                      _buildTextButton(3, '읽고 싶은'),
+                    ],
+                  ),
+                ],
+              ),
             ),
 
             // 실제 보유한 책이 나오는 container
             Flexible(
-              child: Container(
-                color: Colors.grey,
+              flex: 80,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                  ],
+                ),
               ),
-              flex: 8,
             ),
           ],
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextButton(int index, String text) {
+    // body 상단에 들어가는 body 전환버튼 builder
+    // 선택된 버튼의 text 색은 검은색, 선택 안 되면 회색
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          _selectedIndex = index; // 버튼 인덱스 업데이트
+        });
+      },
+      child: Text(
+        text,
+        style: TextStyle(
+          color: _selectedIndex == index ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSegmentContainer(double width, int index) {
+    // 버튼 위의 일직선
+    // 버튼이 선택되면 상단의 일직선도 색과 굵기가 바뀜(selectedIndex 값에 따라 바뀜)
+    return Container(
+        width: width,
+        height: _selectedIndex==index?2:0.5,
+        color: _selectedIndex == index ? Color(0xfff17374) : Colors.grey,
+      );
   }
 }
